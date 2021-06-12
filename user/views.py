5acdc5baf1dafc2cad4755764c7ed575579ec1f7
P513 <psb9663@gmail.com>
2021-06-13 00:18:40 +0900

    add signupview class

--- user/views.py
+++ user/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from user.forms import SignUpForm
+from .models import User
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
 
 
 def index(request):
-    return render('index.html')
+    return render(request, 'index.html')
+
+
+class SignUpView(FormView):
+    template_name = 'signup.html'
+
+    form_class = SignUpForm
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # user DB 추가하는 구문 넣어야하나
+            return HttpResponseRedirect('login.html')
+        return render(request, self.name, {'form': form})
