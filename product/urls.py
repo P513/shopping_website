48f2413d1b8e80fe29c3c50390c673143aea569d
P513 <psb9663@gmail.com>
2021-06-15 13:41:45 +0900

    add product form

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-
+from .views import Product_RegisterView
 from . import views
 
 
@@ -8,5 +7,7 @@ app_name = 'product'
 
 urlpatterns = [
     #    path('questions', views.index, name='index'),
-
+    path('', views.index, name='index'),
+    # path(''), = 등록된 상품들 보기 -> 구매버튼 클릭시 order로
+    path('register/', Product_RegisterView.as_view(), name='register'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import RegisterForm
+from django.views.generic.edit import FormView
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+class Product_RegisterView(FormView):
+    template_name = 'product_register.html'
+
+    form_class = RegisterForm
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return redirect('product:register')
+        else:
+            return render(request, self.template_name, {'form': form})
