40d935ba7491b0c8b38c1c543908510c88c477ed
P513 <psb9663@gmail.com>
2021-06-18 23:12:14 +0900

    add duplicate test

--- user/forms.py
+++ user/forms.py
@@ -16,40 +16,59 @@ class SignUpForm(forms.Form):
         error_messages={'required': '비밀번호를 한번 더 입력해주세요.'}, widget=forms.PasswordInput, label='비밀번호 확인',)
 
     def clean(self):
-        cleaned_data = super().clean()
-        check = 1
-        _id = cleaned_data.get('_id')
-        if _id:
-            if User.objects.filter(_id=_id).exists():
-                self.add_error('_id', '존재하는 아이디입니다.')
-                check = 0
-        nickname = cleaned_data.get('nickname')
-        if nickname:
-            if User.objects.filter(nickname=nickname).exists():
-                self.add_error('nickname', '존재하는 닉네임입니다.')
-                check = 0
-        email = cleaned_data.get('email')
-        if email:
-            if User.objects.filter(email=email).exists():
-                self.add_error('email', '이미 가입된 이메일입니다.')
-                check = 0
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
+        if 'signup' in self.data:
+            cleaned_data = super().clean()
+            check = 1
+            _id = cleaned_data.get('_id')
+            if _id:
+                if User.objects.filter(_id=_id).exists():
+                    self.add_error('_id', '존재하는 아이디입니다.')
+                    check = 0
+            nickname = cleaned_data.get('nickname')
+            if nickname:
+                if User.objects.filter(nickname=nickname).exists():
+                    self.add_error('nickname', '존재하는 닉네임입니다.')
+                    check = 0
+            email = cleaned_data.get('email')
+            if email:
+                if User.objects.filter(email=email).exists():
+                    self.add_error('email', '이미 가입된 이메일입니다.')
+                    check = 0
+            password = cleaned_data.get('password')
+            confirm_password = cleaned_data.get('confirm_password')
 
-        if password and confirm_password:
-            if password != confirm_password:
-                self.add_error('password', '비밀번호가 서로 다릅니다.')
-                self.add_error('confirm_password', '비밀번호가 서로 다릅니다.')
-            else:
-                if check == 1:
-                    user = User(
-                        _id=_id,
-                        nickname=nickname,
-                        email=email,
-                        password=make_password(password),
-                        grade='user',
-                    )
-                    user.save()
+            if password and confirm_password:
+                if password != confirm_password:
+                    self.add_error('password', '비밀번호가 서로 다릅니다.')
+                    self.add_error('confirm_password', '비밀번호가 서로 다릅니다.')
+                else:
+                    if check == 1:
+                        user = User(
+                            _id=_id,
+                            nickname=nickname,
+                            email=email,
+                            password=make_password(password),
+                            grade='user',
+                        )
+                        user.save()
+        elif 'checkID' in self.data:
+            cleaned_data = super().clean()
+            _id = cleaned_data.get('_id')
+            if _id:
+                if User.objects.filter(_id=_id).exists():
+                    self.add_error('_id', '존재하는 아이디입니다.')
+        elif 'checkName' in self.data:
+            cleaned_data = super().clean()
+            nickname = cleaned_data.get('nickname')
+            if nickname:
+                if User.objects.filter(nickname=nickname).exists():
+                    self.add_error('nickname', '존재하는 닉네임입니다.')
+        elif 'checkMail' in self.data:
+            cleaned_data = super().clean()
+            email = cleaned_data.get('email')
+            if email:
+                if User.objects.filter(email=email).exists():
+                    self.add_error('email', '존재하는 이메일입니다.')
 
 
 class LoginForm(forms.Form):
